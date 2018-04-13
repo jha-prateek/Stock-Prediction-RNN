8103ef06800b7d78728fcc060f282511835f11d8
Prateek Jha <prateekj777@gmail.com>
2018-04-13 16:11:33 +0530

    Initial commit

---
+++ stock_RNN.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from keras.layers.core import Dense, Dropout
+from keras.layers.recurrent import GRU
+from keras.models import Sequential, load_model
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import os
+
+prices = pd.read_csv('prices_stock.csv', index_col=['date'])
+
+# selecting YHOO stocks
+yahoo = prices[prices['symbol']=='YHOO']
+
+# preparing input features
+yahoo = yahoo.drop(['symbol'], axis=1)
+yahoo = yahoo.drop(['volume'], axis=1)
+
+# preparing label data
+yahoo_shift = yahoo.shift(-1)
+label = yahoo_shift['close']
+
+# adjusting the shape of both
+yahoo.drop(yahoo.index[len(yahoo)-1], axis=0, inplace=True)
+label.drop(label.index[len(label)-1], axis=0, inplace=True)
+
+# conversion to numpy array
+x, y = yahoo.values, label.values
+
+# scaling values for model
+x_scale = MinMaxScaler()
+y_scale = MinMaxScaler()
+
+X = x_scale.fit_transform(x)
+Y = y_scale.fit_transform(y.reshape(-1,1))
+
+# splitting train and test
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
+X_train = X_train.reshape((-1,1,4))
+X_test = X_test.reshape((-1,1,4))
+
+# creating model using tflearn
+# tf.reset_default_graph()
+
+model_name = 'stock_price_GRU'
+
+# model = Sequential()
+# model.add(GRU(units=512,
+#               return_sequences=True,
+#               input_shape=(1, 4)))
+# model.add(Dropout(0.2))
+# model.add(GRU(units=256))
+# model.add(Dropout(0.2))
+# model.add(Dense(1, activation='sigmoid'))
+# model.compile(loss='mse', optimizer='adam')
+
+model = load_model("{}.h5".format(model_name))
+print("MODEL-LOADED")
+print(model.summary())
+# model.fit(X_train,y_train,batch_size=250, epochs=500, validation_split=0.1)
+# model.save("{}.h5".format(model_name))
+# print('MODEL-SAVED')
+score = model.evaluate(X_test, y_test)
+print('Score: {}'.format(score))
+yhat = model.predict(X_test)
+yhat = y_scale.inverse_transform(yhat)
+y_test = y_scale.inverse_transform(y_test)
+plt.plot(yhat[-100:], label='Predicted')
+plt.plot(y_test[-100:], label='Ground Truth')
+plt.legend()
+plt.show()
